f7395d48d5b7ce1d360775664cce7d041b584348
Sedky Abou-Shamalah <sedkyaboushamalah@gmail.com>
2019-07-24 19:17:25 -0400

    splitting code into files

---
+++ articles.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Article Example Documentation
+type Article struct {
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+// Articles let's declare a global Articles array
+// that we can then populate in our main function
+// to simulate a database
+var Articles []Article
+
+func initArticles() {
+	Articles = []Article{
+		{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	switch vars["id"] {
+	case "1":
+		json.NewEncoder(w).Encode(Articles[0])
+	case "2":
+		json.NewEncoder(w).Encode(Articles[1])
+	default:
+		fmt.Fprintf(w, "bugger off")
+	}
+
+	return
+}
